f9a728b68ecd98f2ac793b76cf276560d546752e
ZukFA <zuk-m@ya.ru>
2020-10-29 20:04:46 +0300

    reworked without unnecessary state.filtered

--- src/components/TodoBuilder.js
+++ src/components/TodoBuilder.js
@@ -7,16 +7,15 @@ class TodoBuilder extends Component{
     constructor(props){
         super(props);
         this.state = {
-            todos: (this.getTodos() || []),
+            todos: (this.getTodosFromStorage() || []),
             msg: '',
             date: '',
             isCompleted: false,
             msgIsEmpty: false,
             sorted: {
-                byNumber: '',
-                byText: ''
+                byNumber: 'desc',
+                byText: 'desc'
             },
-            filtered: (this.getTodos() || []),
             filterBy: '',
             maxPossibleDate: '9999-12-31',
             timePeriod: {
@@ -30,42 +29,35 @@ class TodoBuilder extends Component{
         this.handleDelete = this.handleDelete.bind(this);
         this.handleSort = this.handleSort.bind(this);
         this.handleFilter = this.handleFilter.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
         this.handleTimePeriod = this.handleTimePeriod.bind(this);
     }
-    getTodos = () => JSON.parse(sessionStorage.getItem('todos'));
-    setTodos = todos => sessionStorage.setItem('todos', JSON.stringify(todos));
-    isFiltered() {
+    getTodosFromStorage = () => JSON.parse(sessionStorage.getItem('todos'));
+    saveTodosInStorage = todos => sessionStorage.setItem('todos', JSON.stringify(todos));
+    shouldBeFiltered() {
         if (this.state.filterBy ||
             this.state.timePeriod.start !== '' || 
             this.state.timePeriod.end !== this.state.maxPossibleDate){
             return true;
         } else {
-            console.group(this.state.filterBy, this.state.timePeriod.start, this.state.timePeriod.end)
             return false;
         }
     }
-    handleSubmit(e){ // Если добавляется todo, не удовлетворяющая текущим фильтрам, она все равно отобразится в списке до первой фильтрации.
+    handleSubmit(e){
         e.preventDefault();
-        if (this.state.msg.trim()) {
-            const todosCopy = this.state.todos.slice();
-            const filtered = this.state.filtered.slice();
-            todosCopy.push(
-                {   
-                    msg: this.state.msg,
-                    date: this.state.date, 
-                    id: Date.now(),
-                    isCompleted: false,
-                });
-            if (this.state.filterBy ||  
-                    (this.state.msg.trim().includes(this.state.filterBy) || 
-                    (this.state.date <= this.state.timePeriod.end && this.state.date >= this.state.timePeriod.start))) {
-                filtered.push(todosCopy[todosCopy.length-1]);
-            }
-            this.setState({
-                todos: todosCopy,
-                filtered,
+        if (this.state.msg.trim()){
+            this.setState(state => {
+                let todos = [...state.todos];
+                todos.push(
+                    {   
+                        msg: state.msg,
+                        date: state.date, 
+                        id: Date.now(),
+                        isCompleted: false,
+                    });
+                this.saveTodosInStorage(todos);
+                return { todos }
             });
-            this.setTodos(todosCopy);
         } else {
             this.setState({
                 msgIsEmpty: true,
@@ -79,109 +71,111 @@ class TodoBuilder extends Component{
         });
     }
     handleComplete(id){
-        let todosCopy = this.state.todos.slice();
-        todosCopy = todosCopy.map(todo => {
-            if (todo.id === id) {
-                todo.isCompleted = !todo.isCompleted;
-            }
-            return todo;
+        this.setState(state => {
+            let todos = state.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.isCompleted = !todo.isCompleted;
+                }
+                return todo;
+            });
+            this.saveTodosInStorage(todos);
+            return { todos }
         });
-        this.setState({
-            todos: todosCopy, // В этот момент не меняется значение isCompleted у filtered. И оно не изменится до тех пор, пока в поиске не будет совпадающая строка (см. HandleFilter). Но при вводе любого значения в поиск и после его очистки isCompleted начинает меняться и у filtered, почему?
-        });
-        this.setTodos(todosCopy);
     }
     handleDelete(id){
-        const todosCopy = this.state.todos.filter(item => item.id !== id);
-        const filtered = this.state.filtered.filter(item => item.id !== id);
-        this.setState({
-            todos: todosCopy,
-            filtered,
+        this.setState(state => {
+            const todos = state.todos.filter(item => item.id !== id);
+            this.saveTodosInStorage(todos);
+            return { todos }
         });
-        this.setTodos(todosCopy);
     }
-    handleFilter(e){
-        const filterBy = e.target.value;
-        const filtered = [];
-        this.state.todos.forEach(todo => {
-            console.log(todo.date)
-            if (
-                todo.msg.includes(filterBy) &&
-                (todo.date >= this.state.timePeriod.start && todo.date <= this.state.timePeriod.end)
-                ) {
-                filtered.push(todo);
-            }
+    handleFilter(){
+        let filtered = this.state.todos.filter(todo => {
+            return (todo.msg.includes(this.state.filterBy) && (todo.date >= this.state.timePeriod.start && todo.date <= this.state.timePeriod.end));
         });
+        if (this.state.sorted.byText === 'asc') {
+            filtered.sort((a,b) => a.msg > b.msg ? 1 : -1); // asc
+        } else if (this.state.sorted.byText === 'desc') {
+            filtered.sort((a, b) => a.msg < b.msg ? 1 : -1); // desc
+        } else if (this.state.sorted.byNumber === 'asc') {
+            filtered.sort((a, b) => a.date > b.date ? 1 : -1); // asc
+        } else if (this.state.sorted.byNumber === 'desc') {
+            filtered.sort((a, b) => a.date < b.date ? 1 : -1); // desc
+        }
+        return filtered;
+    }
+    handleSearch(e){
         this.setState({
-            filtered,
-            filterBy,
+            filterBy: e.target.value,
         });
     }
     handleTimePeriod(e){
-        let start = e.target.name === 'start' ? e.target.value : this.state.timePeriod.start;
-        let end = e.target.name === 'end' 
-                ? (e.target.value !== '' ? e.target.value : this.state.maxPossibleDate) 
-                : this.state.timePeriod.end;
-        const todosCopy = this.state.filterBy ? this.state.filtered.slice() : this.state.todos.slice();
-        const filtered = [];
-        todosCopy.forEach(todo => {
-            if (todo.date >= start && todo.date <= end) {
-                filtered.push(todo);
+        e.persist();
+        this.setState(state => {
+            const start = e.target.name === 'start' ? e.target.value : state.timePeriod.start;
+            const end = e.target.name === 'end' 
+                ? (e.target.value !== '' ? e.target.value : state.maxPossibleDate) 
+                : state.timePeriod.end;
+            return {
+                timePeriod: { start, end }
             }
-        })
-        this.setState({
-            timePeriod: {
-                start,
-                end,
-            },
-            filtered,
         });
     }
     handleSort(e){
-        // Не добавил проверку на то, отсортированы ли данные изначально, из-за этого при отсортированных в DESC порядке данных кнопка сработает только со второго раза
-        this.isFiltered = this.isFiltered.bind(this);
-        let todosCopy = (this.isFiltered()) ? this.state.filtered.slice() : this.state.todos.slice();
+        // Не добавил проверку на то, отсортированы ли данные изначально, из-за этого при отсортированных в DESC порядке данных в первый раз произойдет рендер туду в том же порядке
+        this.shouldBeFiltered = this.shouldBeFiltered.bind(this);
+        let todos = (this.shouldBeFiltered() ? this.handleFilter() : [...this.state.todos]);
         if ((e.target.id || e.target.parentNode.id) === 'text'){
-            if (this.state.sorted.byText === ('' || 'asc')) {
-                todosCopy.sort((a, b) => a.msg < b.msg ? 1 : -1); // desc
-                this.setState({
-                    todos: this.isFiltered() ? this.state.todos : todosCopy,
-                    filtered: this.isFiltered() ? todosCopy : this.state.todos,
-                    sorted: {
-                        byText: 'desc',
-                    },
+            if (this.state.sorted.byText === 'asc') {
+                todos.sort((a, b) => a.msg < b.msg ? 1 : -1); // desc
+                this.setState(state => {
+                    return {
+                        sorted: {
+                            byText: 'desc',
+                            byNumber: state.sorted.byNumber,
+                        }
+                    }
                 });
             } else {
-                todosCopy.sort((a,b) => a.msg > b.msg ? 1 : -1); // asc
-                this.setState({
-                    todos: this.isFiltered() ? this.state.todos : todosCopy,
-                    filtered: this.isFiltered() ? todosCopy : this.state.todos,
-                    sorted: {
-                        byText: 'asc',
-                    },
+                todos.sort((a,b) => a.msg > b.msg ? 1 : -1); // asc
+                this.setState(state => {
+                    return {
+                        sorted: {
+                            byText: 'asc',
+                            byNumber: state.sorted.byNumber,
+                        }
+                    }
                 });
             }
         } else {
-            if (this.state.sorted.byNumber === ('' || 'asc')){
-                todosCopy.sort((a, b) => a.date < b.date ? 1 : -1); // desc
-                this.setState({
-                    todos: this.isFiltered() ? this.state.todos : todosCopy,
-                    filtered: this.isFiltered() ? todosCopy : this.state.todos,
-                    sorted: {
-                        byNumber: 'desc',
-                    },
+            if (this.state.sorted.byNumber === 'asc'){
+                todos.sort((a, b) => a.date < b.date ? 1 : -1); // desc
+                this.setState(state => {
+                    return {
+                        sorted: {
+                            byNumber: 'desc',
+                            byText: state.sorted.byText,
+                        },
+                    }
                 });
             } else {
-                todosCopy.sort((a, b) => a.date > b.date ? 1 : -1); // asc
-                this.setState({
-                    todos: this.isFiltered() ? this.state.todos : todosCopy,
-                    filtered: this.isFiltered() ? todosCopy : this.state.todos,
-                    sorted: {
-                        byNumber: 'asc',
-                    },
+                todos.sort((a, b) => a.date > b.date ? 1 : -1); // asc
+                this.setState(state => {
+                    return {
+                        sorted: {
+                            byNumber: 'asc',
+                            byText: state.sorted.byText,
+                        },
+                    }
                 });
             }
         }
+        if (!this.shouldBeFiltered()){
+            this.setState({
+                todos,
+            });
+        }
+        return todos;
     }
     render(){
         return (
@@ -205,14 +199,14 @@ class TodoBuilder extends Component{
                             className='btn'
                         ><b>Add</b></button>
                     </form>
-                    <Search handleChange={this.handleFilter}/>
+                    <Search handleChange={this.handleSearch}/>
                     <Menu 
-                        todos={this.isFiltered ? this.state.filtered : this.state.todos}
+                        todos={this.shouldBeFiltered() ? this.handleFilter() : this.state.todos}
                         handleSort={this.handleSort}
                         handleTimePeriod={this.handleTimePeriod}
                     />
                     <TodoList
-                        todos={this.isFiltered() ? this.state.filtered : this.state.todos}
+                        todos={this.shouldBeFiltered() ? this.handleFilter() : this.state.todos}
                         handleDelete={this.handleDelete}
                         handleComplete={this.handleComplete}
                     />
